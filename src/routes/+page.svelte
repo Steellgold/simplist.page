b867d56753a98d51d0c2bc1d27a37bef7b65bdb1
Gaëtan H <steellgold0@gmail.com>
2023-07-01 01:46:49 +0200

    chore: add .eslintignore file
    chore: add .eslintrc.cjs file
    
    chore: add .gitignore file
    
    chore: add .npmrc file
    
    chore: add README.md file
    
    chore: add package.json file
    
    chore: add postcss.config.js file
    
    chore: add src/app.d.ts file
    
    chore: add src/app.html file
    
    chore: add src/routes/+layout.svelte file
    
    chore: add src/routes/+page.svelte file
    
    chore: add src/tailwind.css file
    
    chore: add static/favicon.png file
    
    chore: add static/simplist.png file
    
    chore: add svelte.config.js file
    
    chore: add tailwind.config.ts file
    
    chore: add tsconfig.json file
    
    chore: add vite.config.ts file

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+
+<!-- centered middle screen -->
+<div class="flex flex-col items-center justify-center h-screen">
+  <img
+    src="/simplist.png"
+    alt="plus"
+    class="h-16 opacity-50" />
+</div>
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "tailwindcss";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+} satisfies Config;
