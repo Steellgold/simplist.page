41adca49a98cc965ca9a0dd8f8494593da8eee1f
Gaëtan H <steellgold0@gmail.com>
2023-07-01 02:08:25 +0200

    style(+page.svelte): update styling of the page component feat(+page.svelte): add input field and button for user interaction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
 
-<!-- centered middle screen -->
-<div class="flex flex-col items-center justify-center h-screen">
+<div class="flex flex-col items-center justify-center mt-20 md:mt-0 px-3 md:px-0 md:h-screen">
   <img
     src="/simplist.png"
     alt="plus"
-    class="h-16 opacity-50" />
+    class="h-16 animate-pulse" />
+
+  <div class="bg-[#1E293B] rounded-full p-1 mt-4 w-full max-w-2xl">
+    <div class="flex">
+      <div class="bg-[#2B3A52] rounded-full p-2">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </div>
+
+      <input
+        type="text"
+        placeholder="Combien y a t&apos;il d&apos;humains sur Terre ?"
+        class="text-[#707F97] w-full placeholder-[#707F97] outline-none ml-2 bg-transparent" />
+
+      <div class="text-[#707F97] p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M10 14l11 -11"></path>
+          <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5"></path>
+       </svg>
+      </div>
+    </div>
+  </div>
 </div>
