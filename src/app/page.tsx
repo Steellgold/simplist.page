ae1927bb755b37c30b8b00faf00073b055623e81
Gaëtan H <steellgold0@gmail.com>
2023-08-01 14:40:16 +0200

    style(page.tsx): remove commented out code and add missing closing tag for div element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,19 +17,14 @@ export default async function Index(): Promise<ReactElement> {
   const { connected } = await getData();
 
   return (
-    <div className="flex flex-col items-center justify-center mt-32 md:mt-0 px-3 md:px-0 md:h-screen">
-      <Image src={"/simplist-light.png"} alt="Simplist logo" quality={5} width={200} height={50} />
+    <>
+      <div className="flex flex-col items-center justify-center mt-32 md:mt-0 px-3 md:px-0 md:h-screen">
+        <Image src={"/simplist-light.png"} alt="Simplist logo" quality={5} width={200} height={50} />
 
-      <div className="flex flex-col max-w-2xl w-full">
-        <SearchBar connected={connected} randomQuestion={randomMessages[Math.floor(Math.random() * randomMessages.length)].question} />
+        <div className="flex flex-col max-w-2xl w-full">
+          <SearchBar connected={connected} randomQuestion={randomMessages[Math.floor(Math.random() * randomMessages.length)].question} />
+        </div>
       </div>
-
-      {/* <div className={clsx(
-          "border-light border-dashed border-2 rounded p-3 mt-4 cursor-pointer hover:bg-[#1E293B]",
-          "opacity-5 hover:opacity-100 transition-opacity duration-300 ease-in-out"
-        )}>
-          <AiFillPlusCircle className="text-light h-8 w-8" />
-        </div> */}
-    </div>
+    </>
   );
 }
