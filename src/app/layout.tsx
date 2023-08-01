da9edda89efc0b3bffef2d940247ae09f5402a72
Gaëtan H <steellgold0@gmail.com>
2023-08-01 03:27:17 +0200

    chore(layout.tsx): add revalidate constant with value 0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import type { PropsWithChildren } from "react";
 
 export { metadata } from "#/lib/configs/metadata";
 
+export const revalidate = 0;
+
 const RootLayout: Component<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="en" suppressHydrationWarning>
