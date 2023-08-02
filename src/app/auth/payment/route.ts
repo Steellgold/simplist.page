e93c334547ce42c255c8924948ec17cec8758715
Gaëtan H <steellgold0@gmail.com>
2023-08-02 11:40:59 +0200

    Update route.ts

--- src/app/auth/payment/route.ts
+++ src/app/auth/payment/route.ts
@@ -15,12 +15,6 @@ export const GET = async(request: NextRequest): Promise<NextResponse> => {
     const schema = z.object({ userId: z.string() }).safeParse(session.metadata);
     if (!schema.success) return NextResponse.redirect(requestUrl.origin); // Lol, this never happens. (I think) (I hope) (I pray)
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const alreadyPaid = await prisma.payments.findFirst({ where: { id: session.id } });
-    if (alreadyPaid) return NextResponse.redirect(requestUrl.origin); // hmmm
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    await prisma.payments.update({ where: { id: session.id }, data: { alreadyApproved: true } });
-
     const data = await prisma.user.update({
       where: { id: schema.data.userId },
       data: { credits: { increment: 100 } }
