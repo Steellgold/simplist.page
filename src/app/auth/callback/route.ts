e9a68b478b2e6b8fc69d379d10cb2373f2faee9c
Gaëtan H <steellgold0@gmail.com>
2023-08-02 18:49:05 +0200

    refactor(auth/callback/route.ts): remove unused variable 'dynamic'

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,8 +5,6 @@ import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 import { prisma } from "#/lib/db/prisma";
 
-export const dynamic = "force-dynamic";
-
 export const GET = async(request: NextRequest): Promise<NextResponse> => {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
